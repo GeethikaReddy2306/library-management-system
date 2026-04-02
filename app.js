40daa00515cdc24fb8f48ed22fd08f6db530b9d1
geethika <geethikareddy051@gmail.com>
2026-04-02 19:13:29 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+// getting-started.js
+const mongoose = require('mongoose');
+
+main().then((res)=>{
+        console.log('coneection successfully')
+})
+
+.catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+const userSchema=new mongoose.Schema({
+        name:String,
+    email:String,
+     rollnumber:Number,
+   year: Number})
