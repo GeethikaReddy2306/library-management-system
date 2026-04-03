690e4d79c5502d6d7c00723b217ccd976b827e43
geethika <geethikareddy051@gmail.com>
2026-04-03 19:46:24 +0530

    your message here

--- app.js
+++ app.js
@@ -22,6 +22,6 @@ const userSchema=new mongoose.Schema({
    const User=mongoose.model("user",userSchema);
     const Employe=mongoose.model("employe",userSchema);
 User.insertMany([{name: "geethika",email:"geethikakotu@gmail.com", rollnumber:29,year:3,booktaken:4,bookpreviousl:0},
-  {name: "kavya",email:"kavya@gmail.com", rollnumber:20,year:3},
+  {name: "kavya",email:"kavya@gmail.com", rollnumber:20,year:3,booktaken:6,bookpreviousl:1},
    {name: "nikitha",email:"nikitha@gmail.com", rollnumber:81,year:3},{name: "deepika",email:"deepika@gmail.com", rollnumber:80,year:3},
   {name: "sai",email:"sai@gmail.com", rollnumber:21,year:2}])
