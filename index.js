efb07e6766c9cac0898fd3c9448aa3a1c5494c87
geethika <geethikareddy051@gmail.com>
2026-04-04 19:14:57 +0530

    your message here

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 const express=require('express');
 const app=express();
+const port=3000;
 const path=require("path");
+const mongoose=require('mongoose');
+main()
+.then(()=>{
+        console.log("connection sucessfully:");
+
+})
+.cath((err)=>console.log("connection successfully"));
+async function name() {
+        await mongoose.connect('mongodb://127.o0.0.1:27017/test')
+}
+app.set('views',path.join(__dirname,"views"));
+app.set("view engine","ejs");
+app.get("/",(req,res)=>{
+        res.send("working root");
+})
